b924118c7fc9ab112104c3bc8e01d2e451a0767f
Griko Nibras <git@griko.id>
2022-07-05 02:19:36 +0700

    fix(example): update wadesta import path

--- packages/example-app/pages/_app.tsx
+++ packages/example-app/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-import { WadestaProvider } from "wadesta";
+import { WadestaProvider } from "wadesta/src";
 
 export default function CustomApp({ Component, pageProps }: AppProps) {
   return (
