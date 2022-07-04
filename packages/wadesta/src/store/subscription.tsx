31437efdd58096dc99e83750c95db4c5944d6ac5
Griko Nibras <git@griko.id>
2022-07-05 01:20:30 +0700

    feat(store): update keplr_keystorechange subscription

--- packages/wadesta/src/store/subscription.tsx
+++ packages/wadesta/src/store/subscription.tsx
@@ -4,15 +4,19 @@ import { connect } from "../actions/account";
 import { useWadestaStore } from ".";
 
 export function WadestaSubscription() {
+  // track keplr_keystorechange
   useEffect(() => {
-    window.addEventListener("keplr_keystorechange", () => void listen());
+    function listen() {
+      const { activeChain } = useWadestaStore.getState();
+      if (activeChain) void connect(activeChain);
+    }
+    window.addEventListener("focus", listen);
+    window.addEventListener("keplr_keystorechange", listen);
+    return () => {
+      window.removeEventListener("focus", listen);
+      window.removeEventListener("keplr_keystorechange", listen);
+    };
   }, []);
-  return null;
-}
 
-async function listen() {
-  const { activeChain } = useWadestaStore.getState();
-  if (activeChain) {
-    await connect(activeChain);
-  }
+  return null;
 }
