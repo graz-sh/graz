aed1f02d14afc0c5b4c93a5e8be0e9d1a57adc8b
Griko Nibras <git@griko.id>
2022-07-05 03:46:16 +0700

    feat(example): add toast on connect/disconnect

--- packages/example-app/pages/index.tsx
+++ packages/example-app/pages/index.tsx
@@ -1,10 +1,29 @@
-import { Button, Stack, Text } from "@chakra-ui/react";
+import { Button, Stack, Text, useToast } from "@chakra-ui/react";
 import { defaultChains, useAccount, useConnect, useDisconnect } from "wadesta/src";
 
 export default function HomePage() {
+  const toast = useToast();
+
   const { data: account, isConnected, isConnecting, isReconnecting, status } = useAccount();
-  const { connect } = useConnect();
-  const { disconnect } = useDisconnect();
+
+  const { connect } = useConnect({
+    onSuccess: (_account) => {
+      toast({
+        status: "success",
+        title: "Wallet connected!",
+        description: `Connected as ${_account.name}`,
+      });
+    },
+  });
+
+  const { disconnect } = useDisconnect({
+    onSuccess: () => {
+      toast({
+        status: "info",
+        title: "Wallet disconnected!",
+      });
+    },
+  });
 
   function handleConnect() {
     return isConnected ? disconnect(undefined) : connect(defaultChains.cosmos);
