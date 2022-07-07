47464e95e64c772b3353e64ecd6bd9db7af33182
Griko Nibras <git@griko.id>
2022-07-07 07:19:28 +0700

    feat(src): move keplr not found registers

--- packages/graz/src/actions/keplr.ts
+++
@@ -1,9 +0,0 @@
-import { useGrazStore } from "../store";
-
-export function registerKeplrNotFound(fn: () => void) {
-  useGrazStore.setState({ _notFoundFn: fn });
-}
-
-export function unregisterKeplrNotFound() {
-  useGrazStore.setState({ _notFoundFn: () => null });
-}
--- packages/graz/src/keplr.ts
+++ packages/graz/src/keplr.ts
@@ -5,3 +5,11 @@ export function getKeplr() {
   useGrazStore.getState()._notFoundFn();
   throw new Error("Keplr is not defined");
 }
+
+export function registerKeplrNotFound(fn: () => void) {
+  useGrazStore.setState({ _notFoundFn: fn });
+}
+
+export function unregisterKeplrNotFound() {
+  useGrazStore.setState({ _notFoundFn: () => null });
+}
