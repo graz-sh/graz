7554c02afcb74912887883fb549e64bf608efeb4
Griko Nibras <git@griko.id>
2022-07-08 05:20:49 +0700

    feat(actions): create client actions

---
+++ packages/graz/src/actions/clients.ts
@@ -0,0 +1,25 @@
+import type { HttpEndpoint, SigningCosmWasmClientOptions } from "@cosmjs/cosmwasm-stargate";
+import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
+import type { OfflineSigner } from "@cosmjs/proto-signing";
+
+import type { GrazChain } from "../chains";
+
+export type CreateClientArgs = Pick<GrazChain, "rpc" | "rpcHeaders">;
+
+export async function createClient({ rpc, rpcHeaders }: CreateClientArgs) {
+  const endpoint: HttpEndpoint = { url: rpc, headers: { ...(rpcHeaders || {}) } };
+  const client = await SigningCosmWasmClient.connect(endpoint);
+  return client;
+}
+
+export type CreateSigningClientArgs = CreateClientArgs & {
+  offlineSigner: OfflineSigner;
+  signerOptions?: SigningCosmWasmClientOptions;
+};
+
+export async function createSigningClient(args: CreateSigningClientArgs) {
+  const { rpc, rpcHeaders, offlineSigner, signerOptions = {} } = args;
+  const endpoint: HttpEndpoint = { url: rpc, headers: { ...(rpcHeaders || {}) } };
+  const signingClient = await SigningCosmWasmClient.connectWithSigner(endpoint, offlineSigner, signerOptions);
+  return signingClient;
+}
