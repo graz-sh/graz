126eded014f437994d7b077a8f3f02162daabbe9
Nur Fikri <codingki@gmail.com>
2022-07-09 00:52:01 +0700

    feat(example): update chains import

--- example/ui/chain-switcher.tsx
+++ example/ui/chain-switcher.tsx
@@ -1,6 +1,5 @@
 import { Button, ButtonGroup, FormControl, FormLabel, useToast } from "@chakra-ui/react";
-import { OSMOSIS_TESTNET } from "constants/osmosis-testnet";
-import { defaultChainsArray, useAccount, useConnect, useSuggestChainAndConnect } from "graz";
+import { mainnetChainsArray, testnetChains, useAccount, useConnect, useSuggestChainAndConnect } from "graz";
 
 export function ChainSwitcher() {
   const toast = useToast();
@@ -29,7 +28,7 @@ export function ChainSwitcher() {
     <FormControl>
       <FormLabel>Switch Chain</FormLabel>
       <ButtonGroup isDisabled={isConnecting || isReconnecting} size="sm">
-        {defaultChainsArray.map((chain) => (
+        {mainnetChainsArray.map((chain) => (
           <Button key={chain.chainId} onClick={() => connect(chain)}>
             {chain.chainId}
           </Button>
@@ -37,7 +36,7 @@ export function ChainSwitcher() {
       </ButtonGroup>
       <FormLabel mt={2}>Suggest and connect chain</FormLabel>
       <ButtonGroup isDisabled={isConnecting || isReconnecting} size="sm">
-        <Button onClick={() => suggestAndConnect(OSMOSIS_TESTNET)}>{OSMOSIS_TESTNET.chainId}</Button>
+        <Button onClick={() => suggestAndConnect(testnetChains.osmosis)}>{testnetChains.osmosis.chainId}</Button>
       </ButtonGroup>
     </FormControl>
   );
