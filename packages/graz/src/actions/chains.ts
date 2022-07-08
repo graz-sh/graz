8b9ddf18600dff2718f8c85d4346e6f4d3a9b29c
Nur Fikri <codingki@gmail.com>
2022-07-08 17:04:21 +0700

    feat(hooks): add useSuggestChainAndConnect

--- packages/graz/src/actions/chains.ts
+++ packages/graz/src/actions/chains.ts
@@ -1,9 +1,16 @@
 import type { ChainInfo } from "@keplr-wallet/types";
 
 import { getKeplr } from "../keplr";
+import { connect } from "./account";
 
 export async function suggestChain(chainInfo: ChainInfo) {
   const keplr = getKeplr();
   await keplr.experimentalSuggestChain(chainInfo);
+  return chainInfo;
+}
+
+export async function suggestChainAndConnect(chainInfo: ChainInfo) {
+  await suggestChain(chainInfo);
+  await connect(chainInfo);
   return chainInfo;
 }
--- packages/graz/src/hooks/chains.ts
+++ packages/graz/src/hooks/chains.ts
@@ -1,7 +1,7 @@
 import type { ChainInfo } from "@keplr-wallet/types";
 import { useMutation } from "react-query";
 
-import { suggestChain } from "../actions/chains";
+import { suggestChain, suggestChainAndConnect } from "../actions/chains";
 import { useGrazStore } from "../store";
 import type { MutationEventArgs } from "../types/hooks";
 
@@ -25,6 +25,26 @@ export function useSuggestChain({ onError, onLoading, onSuccess }: UseSuggestCha
     isSuccess: mutation.isSuccess,
     suggest: mutation.mutate,
     suggestAsync: mutation.mutateAsync,
+    status: mutation.status,
+  };
+}
+
+export type UseSuggestChainAndConnectArgs = MutationEventArgs<ChainInfo>;
+
+export function useSuggestChainAndConnect({ onError, onLoading, onSuccess }: UseSuggestChainAndConnectArgs = {}) {
+  const queryKey = ["WADESTA_USE_SUGGEST_CHAIN_AND_CONNECT", onError, onLoading, onSuccess];
+  const mutation = useMutation(queryKey, suggestChainAndConnect, {
+    onError: (err, chainInfo) => Promise.resolve(onError?.(err, chainInfo)),
+    onMutate: onLoading,
+    onSuccess: (chainInfo) => Promise.resolve(onSuccess?.(chainInfo)),
+  });
+
+  return {
+    error: mutation.error,
+    isLoading: mutation.isLoading,
+    isSuccess: mutation.isSuccess,
+    suggestAndConnect: mutation.mutate,
+    suggestAndConnectAsync: mutation.mutateAsync,
     status: mutation.status,
   };
 }
