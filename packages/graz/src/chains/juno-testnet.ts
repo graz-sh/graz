e41459d06992e7b7394c46db9606bca4b29aa05a
CamelJuno <98026097+CamelJuno@users.noreply.github.com>
2022-07-15 01:28:15 +0200

    feat(chains): add juno testnet (#15)

---
+++ packages/graz/src/chains/juno-testnet.ts
@@ -0,0 +1,27 @@
+import { Bech32Address } from "@keplr-wallet/cosmos";
+import type { ChainInfo } from "@keplr-wallet/types";
+
+const JUNO = {
+  coinDenom: "junox",
+  coinMinimalDenom: "ujunox",
+  coinDecimals: 6,
+  coinGeckoId: "juno-network",
+  coinImageUrl: "https://raw.githubusercontent.com/cosmos/chain-registry/master/juno/images/juno.png",
+};
+
+const currencies = [JUNO];
+
+export const junoTestnet: ChainInfo = {
+  rpc: "https://rpc.uni.junonetwork.io",
+  rest: "https://api.uni.junonetwork.io",
+  chainId: "uni-3",
+  chainName: "Juno Testnet",
+  stakeCurrency: JUNO,
+  bip44: {
+    coinType: 118,
+  },
+  bech32Config: Bech32Address.defaultBech32Config("juno"),
+  currencies,
+  feeCurrencies: [JUNO],
+  coinType: 118,
+};
