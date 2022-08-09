313ce27b996fbd52e6e6bab0d9285fd407cb072f
Griko Nibras <git@griko.id>
2022-08-10 02:39:39 +0700

    feat(src): update configureGraz

--- packages/graz/src/actions/configure.ts
+++ packages/graz/src/actions/configure.ts
@@ -1,22 +1,23 @@
 import type { GrazChain } from "../chains";
+import type { GrazStore } from "../store";
 import { useGrazStore } from "../store";
 import { registerKeplrNotFound } from "./keplr";
 
 export interface ConfigureGrazArgs {
   defaultChain?: GrazChain;
+  defaultSigningClient?: GrazStore["defaultSigningClient"];
   onKeplrNotFound?: () => void;
 }
 
 export function configureGraz(args: ConfigureGrazArgs = {}): ConfigureGrazArgs {
-  const { defaultChain, onKeplrNotFound } = args;
-
-  if (defaultChain) {
-    useGrazStore.setState({ defaultChain });
+  if (args.defaultChain) {
+    useGrazStore.setState({ defaultChain: args.defaultChain });
   }
-
-  if (onKeplrNotFound) {
-    registerKeplrNotFound(onKeplrNotFound);
+  if (args.defaultSigningClient) {
+    useGrazStore.setState({ defaultSigningClient: args.defaultSigningClient });
   }
-
+  if (args.onKeplrNotFound) {
+    registerKeplrNotFound(args.onKeplrNotFound);
+  }
   return args;
 }
