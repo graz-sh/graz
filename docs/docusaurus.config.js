6b44c628a1777f3ca800c981723e826b49d59008
Nur Fikri <codingki@gmail.com>
2022-08-10 17:21:46 +0700

    fix(docs): edit url

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -34,15 +34,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/strangelove-ventures/graz/tree/dev/docs/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
