435fbe8c48cfc3c806b2c1a4e495468a2988a27b
Griko Nibras <git@griko.id>
2022-09-07 00:51:59 +0700

    feat(provider): add configure props

--- packages/graz/src/provider/index.tsx
+++ packages/graz/src/provider/index.tsx
@@ -1,13 +1,17 @@
 import type { QueryClientProviderProps } from "@tanstack/react-query";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import type { ConfigureGrazArgs } from "../actions/configure";
+import { configureGraz } from "../actions/configure";
 import { GrazEvents } from "./events";
 
 const queryClient = new QueryClient({
   //
 });
 
-export type GrazProviderProps = Partial<QueryClientProviderProps>;
+export type GrazProviderProps = Partial<QueryClientProviderProps> & {
+  grazOptions?: ConfigureGrazArgs;
+};
 
 /**
  * Provider component which extends `@tanstack/react-query`'s {@link QueryClientProvider} with built-in query client
@@ -27,7 +31,10 @@ export type GrazProviderProps = Partial<QueryClientProviderProps>;
  *
  * @see https://tanstack.com/query
  */
-export function GrazProvider({ children, ...props }: GrazProviderProps): JSX.Element {
+export function GrazProvider({ children, grazOptions, ...props }: GrazProviderProps): JSX.Element {
+  if (grazOptions) {
+    configureGraz(grazOptions);
+  }
   return (
     <QueryClientProvider client={queryClient} {...props}>
       <GrazEvents />
