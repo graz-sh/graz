690bebb31c523a9abb1654ee4af18b73d057b1d8
Griko Nibras <git@griko.id>
2022-09-10 23:52:51 +0700

    feat(wallet): add defaultWallet option on configureGraz

--- packages/graz/src/actions/configure.ts
+++ packages/graz/src/actions/configure.ts
@@ -1,10 +1,12 @@
 import type { GrazChain } from "../chains";
 import type { GrazStore } from "../store";
 import { useGrazStore } from "../store";
+import type { WalletType } from "../types/core";
 
 export interface ConfigureGrazArgs {
   defaultChain?: GrazChain;
   defaultSigningClient?: GrazStore["defaultSigningClient"];
+  defaultWallet?: WalletType;
   onNotFound?: () => void;
 }
 
@@ -12,6 +14,7 @@ export function configureGraz(args: ConfigureGrazArgs = {}): ConfigureGrazArgs {
   useGrazStore.setState((prev) => ({
     defaultChain: args.defaultChain || prev.defaultChain,
     defaultSigningClient: args.defaultSigningClient || args.defaultSigningClient,
+    walletType: args.defaultWallet || prev.walletType,
     _notFoundFn: args.onNotFound || prev._notFoundFn,
   }));
   return args;
