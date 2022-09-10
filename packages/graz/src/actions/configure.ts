b26a3c46873a71167547783e9a558343739742e5
Griko Nibras <git@griko.id>
2022-09-10 23:52:26 +0700

    refactor(actions): simplify configureGraz action

--- packages/graz/src/actions/configure.ts
+++ packages/graz/src/actions/configure.ts
@@ -1,23 +1,18 @@
 import type { GrazChain } from "../chains";
 import type { GrazStore } from "../store";
 import { useGrazStore } from "../store";
-import { registerKeplrNotFound } from "./keplr";
 
 export interface ConfigureGrazArgs {
   defaultChain?: GrazChain;
   defaultSigningClient?: GrazStore["defaultSigningClient"];
-  onKeplrNotFound?: () => void;
+  onNotFound?: () => void;
 }
 
 export function configureGraz(args: ConfigureGrazArgs = {}): ConfigureGrazArgs {
-  if (args.defaultChain) {
-    useGrazStore.setState({ defaultChain: args.defaultChain });
-  }
-  if (args.defaultSigningClient) {
-    useGrazStore.setState({ defaultSigningClient: args.defaultSigningClient });
-  }
-  if (args.onKeplrNotFound) {
-    registerKeplrNotFound(args.onKeplrNotFound);
-  }
+  useGrazStore.setState((prev) => ({
+    defaultChain: args.defaultChain || prev.defaultChain,
+    defaultSigningClient: args.defaultSigningClient || args.defaultSigningClient,
+    _notFoundFn: args.onNotFound || prev._notFoundFn,
+  }));
   return args;
 }
