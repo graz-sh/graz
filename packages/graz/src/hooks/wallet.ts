ea0cbf21ec9648b3b31b93274b7e227c8a364d9f
Griko Nibras <git@griko.id>
2022-09-11 00:55:33 +0700

    feat(hooks): add useCheckWallet

--- packages/graz/src/hooks/wallet.ts
+++ packages/graz/src/hooks/wallet.ts
@@ -1,4 +1,8 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { checkWallet } from "../actions/wallet";
 import { useGrazStore } from "../store";
+import type { WalletType } from "../types/core";
 
 /**
  * graz hook which returns boolean whether Keplr Wallet is supported
@@ -18,4 +22,21 @@ import { useGrazStore } from "../store";
  */
 export function useCheckKeplr() {
   return useGrazStore((x) => x._supported);
+}
+
+/**
+ * graz query hook to check whether given {@link WalletType} or default configured wallet is supported
+ *
+ * @example
+ * ```ts
+ * import { useCheckWallet } from "graz";
+ *
+ * const { data: isSupported } = useCheckWallet();
+ * const { data: isKeplrSupported } = useCheckWallet("keplr");
+ * ```
+ */
+export function useCheckWallet(type?: WalletType) {
+  const walletType = useGrazStore((x) => type || x.walletType);
+  const queryKey = ["USE_CHECK_WALLET", walletType] as const;
+  return useQuery(queryKey, ({ queryKey: [, _type] }) => checkWallet(_type));
 }
