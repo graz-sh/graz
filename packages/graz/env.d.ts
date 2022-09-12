32d2f11c1694d6e8def27a74f0c8ce032a5400cf
Griko Nibras <git@griko.id>
2022-09-12 23:33:42 +0700

    feat(env): add window.leap declaration

--- packages/graz/env.d.ts
+++ packages/graz/env.d.ts
@@ -1,6 +1,5 @@
 type KeplrWindow = import("@keplr-wallet/types").Window;
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 declare interface Window extends KeplrWindow {
-  //
+  leap: KeplrWindow["keplr"];
 }
