b04c3194371a85b814769f0e8a38d4e7324f0a6f
Griko Nibras <git@griko.id>
2022-10-06 21:37:04 +0700

    feat: update eslint configs

--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -4,12 +4,8 @@ const { getTsconfigPath } = require("@strangelovelabs/style-guide/eslint/helpers
 
 /** @type {import("eslint").Linter.Config} */
 const eslintConfig = {
-  env: {
-    browser: true,
-    node: true,
-  },
   extends: [
-    require.resolve("@strangelovelabs/style-guide/eslint/base"),
+    require.resolve("@strangelovelabs/style-guide/eslint/browser-node"),
     require.resolve("@strangelovelabs/style-guide/eslint/react"),
     require.resolve("@strangelovelabs/style-guide/eslint/next"),
     require.resolve("@strangelovelabs/style-guide/eslint/typescript"),
--- packages/grazkit-chakra/.eslintrc.js
+++ packages/grazkit-chakra/.eslintrc.js
@@ -4,12 +4,8 @@ const { getTsconfigPath } = require("@strangelovelabs/style-guide/eslint/helpers
 
 /** @type {import("eslint").Linter.Config} */
 const eslintConfig = {
-  env: {
-    browser: true,
-    node: true,
-  },
   extends: [
-    require.resolve("@strangelovelabs/style-guide/eslint/base"),
+    require.resolve("@strangelovelabs/style-guide/eslint/browser-node"),
     require.resolve("@strangelovelabs/style-guide/eslint/react"),
     require.resolve("@strangelovelabs/style-guide/eslint/typescript"),
   ],
@@ -17,18 +13,6 @@ const eslintConfig = {
   parserOptions: {
     project: getTsconfigPath(),
   },
-  rules: {
-    "no-console": ["warn"],
-    "prefer-named-capture-group": ["warn"],
-  },
-  overrides: [
-    {
-      files: ["tsup.config.{js,ts}"],
-      rules: {
-        "import/no-default-export": ["off"],
-      },
-    },
-  ],
   root: true,
 };
 
