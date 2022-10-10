45399520ec7d15331a5629e6af35aebfa4ffa926
Nur Fikri <codingki@gmail.com>
2022-10-11 05:04:25 +0700

    fix: component props

--- templates/default/src/ui/navbar/index.tsx
+++ templates/default/src/ui/navbar/index.tsx
@@ -1,11 +1,12 @@
+import type { ContainerProps } from "@chakra-ui/react";
 import { Container, HStack } from "@chakra-ui/react";
 
 import { NavbarLeftSection } from "./left-section";
 import { NavbarRightSection } from "./right-section";
 
-export const Navbar = () => {
+export const Navbar = (props: ContainerProps) => {
   return (
-    <Container maxWidth="4xl" pt={6} pb={3}>
+    <Container maxWidth="4xl" pt={6} pb={3} {...props}>
       <HStack justifyContent="space-between" width="full">
         <NavbarLeftSection />
         <NavbarRightSection />
