229dfb1b4fb41d8a48c12f3941a159fd2736316f
Nur Fikri <codingki@gmail.com>
2022-10-11 05:01:12 +0700

    chore: improve assets page

--- templates/default/src/pages/assets.tsx
+++ templates/default/src/pages/assets.tsx
@@ -1,5 +1,6 @@
 import { Center, Spinner, Stack, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
 import { useBalances, useBalanceStaked } from "graz";
+import { numberWithCommas } from "src/utils/numberWithCommas";
 
 const Assets = () => {
   const balances = useBalances();
@@ -11,30 +12,34 @@ const Assets = () => {
         <Table variant="simple">
           <Thead>
             <Tr>
-              <Th>Asset</Th>
+              <Th>Asset denom</Th>
               <Th isNumeric>Staked</Th>
               <Th isNumeric>Available</Th>
             </Tr>
           </Thead>
           <Tbody>
-            {balances.data?.map((balance) => (
-              <Tr key={balance.denom}>
-                <Td>{balance.denom}</Td>
-                <Td isNumeric>
-                  {/* eslint-disable-next-line no-nested-ternary */}
-                  {balanceStaked.data?.denom === balance.denom ? (
-                    balanceStaked.isLoading ? (
-                      <Spinner />
+            {balances.data?.map((balance) => {
+              return (
+                <Tr key={balance.denom}>
+                  <Td>{balance.denom}</Td>
+                  <Td fontFamily="mono" isNumeric>
+                    {/* eslint-disable-next-line no-nested-ternary */}
+                    {balanceStaked.data?.denom === balance.denom ? (
+                      balanceStaked.isLoading ? (
+                        <Spinner />
+                      ) : (
+                        numberWithCommas(balanceStaked.data.amount)
+                      )
                     ) : (
-                      balanceStaked.data.amount
-                    )
-                  ) : (
-                    "-"
-                  )}
-                </Td>
-                <Td isNumeric>{balance.amount}</Td>
-              </Tr>
-            ))}
+                      "-"
+                    )}
+                  </Td>
+                  <Td fontFamily="mono" isNumeric>
+                    {numberWithCommas(balance.amount)}
+                  </Td>
+                </Tr>
+              );
+            })}
           </Tbody>
         </Table>
 
