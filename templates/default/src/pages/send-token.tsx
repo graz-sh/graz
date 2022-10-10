aea7841334adbe9c76727232e93676f1c4ba0f1a
Nur Fikri <codingki@gmail.com>
2022-10-11 03:18:40 +0700

    chore: Stack as from

--- templates/default/src/pages/send-token.tsx
+++ templates/default/src/pages/send-token.tsx
@@ -4,35 +4,33 @@ const SendToken = () => {
   return (
     <Stack w="full" spacing={6}>
       <Heading>Send Token</Heading>
-      <form>
-        <Stack spacing={4}>
-          <FormControl>
-            <FormLabel>Coin</FormLabel>
-            <Select placeholder="Select option">
-              <option value="SOMM">SOMM</option>
-            </Select>
-          </FormControl>
-          <FormControl>
-            <FormLabel>Sender address</FormLabel>
-            <Input type="text" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Recipient address</FormLabel>
-            <Input type="text" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Amount</FormLabel>
-            <Input type="text" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Memo</FormLabel>
-            <Input type="text" />
-          </FormControl>
-          <Button width="full" mt={4} type="submit">
-            Send
-          </Button>
-        </Stack>
-      </form>
+      <Stack spacing={4} as="form">
+        <FormControl>
+          <FormLabel>Coin</FormLabel>
+          <Select placeholder="Select option">
+            <option value="SOMM">SOMM</option>
+          </Select>
+        </FormControl>
+        <FormControl>
+          <FormLabel>Sender address</FormLabel>
+          <Input type="text" />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Recipient address</FormLabel>
+          <Input type="text" />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Amount</FormLabel>
+          <Input type="text" />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Memo</FormLabel>
+          <Input type="text" />
+        </FormControl>
+        <Button width="full" mt={4} type="submit">
+          Send
+        </Button>
+      </Stack>
     </Stack>
   );
 };
