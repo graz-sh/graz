2f9285110a2306f44973e2aa7162817eb987cb6b
Burnt <burnt@Admins-MBP-2.fios-router.home>
2022-11-07 10:38:08 -0500

    instantiate smart contract hook implementation

--- packages/graz/src/hooks/methods.ts
+++ packages/graz/src/hooks/methods.ts
@@ -1,8 +1,14 @@
+import type { InstantiateResult } from "@cosmjs/cosmwasm-stargate";
 import type { DeliverTxResponse } from "@cosmjs/stargate";
 import { useMutation } from "@tanstack/react-query";
 
-import type { SendIbcTokensArgs, SendTokensArgs } from "../actions/methods";
-import { sendIbcTokens, sendTokens } from "../actions/methods";
+import type {
+  InstantiateContractArgs,
+  InstantiateContractMutationArgs,
+  SendIbcTokensArgs,
+  SendTokensArgs,
+} from "../actions/methods";
+import { instantiateContract, sendIbcTokens, sendTokens } from "../actions/methods";
 import type { MutationEventArgs } from "../types/hooks";
 import { useAccount } from "./account";
 
@@ -96,6 +102,47 @@ export const useSendIbcTokens = ({
     isSuccess: mutation.isSuccess,
     sendIbcTokens: mutation.mutate,
     sendIbcTokensAsync: mutation.mutateAsync,
+    status: mutation.status,
+  };
+};
+
+export type UseInstantiateContractArgs<Message extends Record<string, unknown>> = {
+  codeId: number;
+} & MutationEventArgs<InstantiateContractMutationArgs<Message>, InstantiateResult>;
+
+export const useInstantiateContract = <Message extends Record<string, unknown>>({
+  codeId,
+  onError,
+  onLoading,
+  onSuccess,
+}: UseInstantiateContractArgs<Message>) => {
+  const { data: account } = useAccount();
+  const accountAddress = account?.bech32Address;
+
+  const mutationFn = (args: InstantiateContractMutationArgs<Message>) => {
+    const contractArgs: InstantiateContractArgs<Message> = {
+      ...args,
+      fee: args.fee ?? "auto",
+      senderAddress: accountAddress,
+      codeId,
+    };
+
+    return instantiateContract(contractArgs);
+  };
+
+  const queryKey = ["USE_INSTANTIATE_CONTRACT", onError, onLoading, onSuccess, codeId];
+  const mutation = useMutation(queryKey, mutationFn, {
+    onError: (err, data) => Promise.resolve(onError?.(err, data)),
+    onMutate: onLoading,
+    onSuccess: (txResponse) => Promise.resolve(onSuccess?.(txResponse)),
+  });
+
+  return {
+    error: mutation.error,
+    isLoading: mutation.isLoading,
+    isSuccess: mutation.isSuccess,
+    instantiateContract: mutation.mutate,
+    instantiateContractAsync: mutation.mutateAsync,
     status: mutation.status,
   };
 };
