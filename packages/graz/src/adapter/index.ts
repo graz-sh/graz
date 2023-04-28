d906887dd751386fd412ad3fec5fde584bad5b31
Nur Fikri <codingki@gmail.com>
2023-04-28 23:43:58 +0700

    init

--- packages/graz/src/adapter/index.ts
+++ packages/graz/src/adapter/index.ts
@@ -1,4 +1,21 @@
-export interface GrazAdapter {
+import type { OfflineSigner } from "@cosmjs/launchpad";
+import type { OfflineDirectSigner } from "@cosmjs/proto-signing";
+
+export interface AccountData {
+  address: Uint8Array;
+  bech32Address: string;
+  pubKey: Uint8Array;
+}
+
+export interface Connector {
+  checkConnector: () => boolean;
+  getAccount: (chainId: string, prefix?: string) => Promise<AccountData>;
+  getOfflineSigner: (chainId: string) => OfflineSigner & OfflineDirectSigner;
+  getOfflineSignerOnlyAmino: (chainId: string) => OfflineSigner;
+  getOfflineSignerAuto: (chainId: string) => Promise<OfflineSigner | OfflineDirectSigner>;
+}
+
+export interface GrazAdapter extends Connector {
   name: string;
   id: string;
 }
