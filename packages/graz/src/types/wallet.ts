5d1c920ec82dd6047a37141f6616c534751bf71b
Nur Fikri <codingki@gmail.com>
2023-05-13 03:45:48 +0700

    feat: support cosmostation

--- packages/graz/src/types/wallet.ts
+++ packages/graz/src/types/wallet.ts
@@ -1,10 +1,7 @@
 export enum WalletType {
   KEPLR = "keplr",
   LEAP = "leap",
+  COSMOSTATION = "cosmostation",
 }
 
-export const WALLET_TYPES = [
-  WalletType.KEPLR,
-  WalletType.LEAP,
-  //
-];
+export const WALLET_TYPES = [WalletType.KEPLR, WalletType.LEAP, WalletType.COSMOSTATION];
