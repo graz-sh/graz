326c17c9c99f31a5da1f4de4d86ae0025792bca3
Griko Nibras <griko@nibras.co>
2023-06-17 04:12:43 +0700

    feat: add useActiveWalletType hook

--- packages/graz/src/hooks/wallet.ts
+++ packages/graz/src/hooks/wallet.ts
@@ -1,9 +1,35 @@
 import type { UseQueryResult } from "@tanstack/react-query";
 import { useQuery } from "@tanstack/react-query";
+import { shallow } from "zustand/shallow";
 
 import { checkWallet } from "../actions/wallet";
 import { useGrazInternalStore } from "../store";
-import type { WalletType } from "../types/wallet";
+import { WalletType } from "../types/wallet";
+
+/**
+ * graz hook to retrieve current active {@link WalletType}
+ *
+ * @example
+ * ```ts
+ * import { useActiveWalletType } from "graz";
+ * const { walletType } = useActiveWalletType();
+ * ```
+ */
+export const useActiveWalletType = () => {
+  return useGrazInternalStore(
+    (x) => ({
+      walletType: x.walletType,
+      isCosmostation: x.walletType === WalletType.COSMOSTATION,
+      isCosmostationMobile: x.walletType === WalletType.WC_COSMOSTATION_MOBILE,
+      isKeplr: x.walletType === WalletType.KEPLR,
+      isKeplrMobile: x.walletType === WalletType.WC_KEPLR_MOBILE,
+      isLeap: x.walletType === WalletType.LEAP,
+      isLeapMobile: x.walletType === WalletType.WC_LEAP_MOBILE,
+      isWalletConnect: x.walletType === WalletType.WALLETCONNECT,
+    }),
+    shallow,
+  );
+};
 
 /**
  * graz query hook to check whether given {@link WalletType} or default configured wallet is supported
