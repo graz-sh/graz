b862a2accb3e11a4d7846ce4b81c0b548560b6f3
Nur Fikri <codingki@gmail.com>
2023-08-28 08:46:08 +0700

    chore: update eslint rules

--- packages/graz/.eslintrc.js
+++ packages/graz/.eslintrc.js
@@ -3,6 +3,9 @@
 const { extendEslint } = require("@graz-sh/style-guide");
 
 module.exports = extendEslint(["browser-node", "react", "typescript", "tsup"], {
+  rules: {
+    "@typescript-eslint/no-non-null-assertion": "warn",
+  },
   ignorePatterns: ["chains/**", "compiled/**", "dist/**"],
   root: true,
 });
