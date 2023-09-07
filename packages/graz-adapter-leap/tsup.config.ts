8222b85e425cf68ab98077b2ddfbd0fc3c34e2a8
Griko Nibras <griko@nibras.co>
2023-09-08 01:16:08 +0700

    feat: update tsup configs

--- packages/graz-adapter-leap/tsup.config.ts
+++ packages/graz-adapter-leap/tsup.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig(({ watch }) => ({
+  cjsInterop: true,
   clean: !watch,
   dts: true,
   entry: ["src/*.ts"],
   format: ["cjs", "esm"],
   minify: !watch,
-  minifyIdentifiers: !watch,
-  minifySyntax: !watch,
-  minifyWhitespace: !watch,
+  shims: true,
+  splitting: true,
+  treeshake: true,
 }));
