caefc34f334b848c1bbd9896af198d903fe73a81
Nur Fikri <codingki@gmail.com>
2023-09-08 17:32:44 +0700

    wip: rewrite starter

--- example/starter/src/pages/index.tsx
+++ example/starter/src/pages/index.tsx
@@ -1,34 +1,10 @@
-import { Heading, List, ListItem, Stack } from "@chakra-ui/react";
-import { useAccount } from "graz";
-
-import { ChainSwitcher } from "../ui/wallet/chain-switcher";
-import { RecentChain } from "../ui/wallet/recent-chain";
+import { Stack } from "@chakra-ui/react";
+import { Card } from "src/ui/card/chain";
 
 const HomePage = () => {
-  const {
-    data: accountData,
-    isConnecting,
-    isReconnecting,
-  } = useAccount({
-    multiChain: true,
-  });
-  console.log("ccc", accountData);
   return (
-    <Stack w="full" gap={2}>
-      {!accountData && (isConnecting || isReconnecting) ? <RecentChain /> : null}
-      {accountData ? (
-        <>
-          <Heading size="lg">Chains: </Heading>
-          <List>
-            {Object.entries(accountData).map(([chainId, account]) => (
-              <ListItem key={chainId}>
-                {chainId} - address: <b>{account?.bech32Address}</b>
-              </ListItem>
-            ))}
-          </List>
-          <ChainSwitcher />
-        </>
-      ) : null}
+    <Stack spacing={4}>
+      <Card />
     </Stack>
   );
 };
