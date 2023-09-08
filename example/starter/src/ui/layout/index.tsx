5a36d0f6c7021f91d5521dfb08e1a3b0ee5e3a6c
Nur Fikri <codingki@gmail.com>
2023-09-08 17:32:44 +0700

    wip: rewrite starter

--- example/starter/src/ui/layout/index.tsx
+++ example/starter/src/ui/layout/index.tsx
@@ -1,35 +1,68 @@
-import { Box, Center, Container, Divider, Heading, Stack, Text } from "@chakra-ui/react";
-import { useAccount } from "graz";
+import { Box, Center, Heading, HStack, keyframes, Link, Stack, Text } from "@chakra-ui/react";
 import Head from "next/head";
+import NextLink from "next/link";
 import type { ReactNode } from "react";
 
-import { Navbar } from "../navbar";
+import { ToggleTheme } from "../core/toggle-theme";
+
+const glowAnimation = keyframes`100% {
+    box-shadow: 0 0 3px #dff8e3, 0 0 10px #dff8e3, 0 0 20px #dff8e3, 0 0 40px #b7fcc2,
+      0 0 70px #b7fcc2, 0 0 80px #b7fcc2;
+  }`;
 
 export const Layout = ({ children }: { children: ReactNode }) => {
-  const { isConnecting, isReconnecting, isConnected } = useAccount();
-
   return (
     <Box>
       <Head>
         <meta content="minimum-scale=1, initial-scale=1, width=device-width" name="viewport" />
       </Head>
-      <Stack>
-        <Navbar />
-        <Divider />
-        <Center>
-          <Container maxW="4xl" mx="auto" pt={4}>
-            {isConnecting || isReconnecting ? <Text>Connecting...</Text> : null}
-            {!isConnected && !(isConnecting || isReconnecting) ? (
-              <Box>
-                <Heading>Welcome to Graz Example Starter</Heading>
-                <Text>Connect your wallet to interact within the app.</Text>
-              </Box>
-            ) : (
-              children
-            )}
-          </Container>
-        </Center>
-      </Stack>
+
+      <Center h="100vh">
+        <Stack
+          bgColor="whiteAlpha.100"
+          borderRadius={12}
+          justifyContent="space-between"
+          minH="200px"
+          mx={2}
+          p={8}
+          px={6}
+          spacing={4}
+          sx={{
+            boxShadow: `0 0 2px #dff8e3, 0 0 10px #dff8e3, 0 0 20px #b7fcc2, 0 0 30px #b7fcc2,
+    0 0 40px #b7fcc2, 0 0 50px #b7fcc2`,
+            animation: `${glowAnimation} 3s infinite linear alternate`,
+          }}
+          w="container.md"
+        >
+          <HStack alignItems="start" justifyContent="space-between">
+            <Stack spacing={1}>
+              <Heading size="lg">Graz Playground</Heading>
+              <Text fontWeight="semibold">Everything you need to start working with the Cosmos ecosystem.</Text>
+            </Stack>
+            <ToggleTheme />
+          </HStack>
+          {children}
+          <Center bottom={0}>
+            <HStack spacing={4} wrap="wrap">
+              <Link as={NextLink} href="https://graz.sh" isExternal>
+                Documentation
+              </Link>
+              <Link as={NextLink} href="https://github.com/graz-sh/graz" isExternal>
+                Graz Github Repo
+              </Link>
+              <Link as={NextLink} href="https://github.com/graz-sh/graz/tree/dev/example/starter" isExternal>
+                This Code
+              </Link>
+              <Link as={NextLink} href="https://twitter.com/graz_sh" isExternal>
+                Twitter
+              </Link>
+              <Link as={NextLink} href="https://discord.gg/2RkzXyjxgt" isExternal>
+                Discord
+              </Link>
+            </HStack>
+          </Center>
+        </Stack>
+      </Center>
     </Box>
   );
 };
