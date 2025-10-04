146a4476f0b6cdbac2a13d4b70c6a5ed5057723d
Nur Fikri <codingki@gmail.com>
2025-10-04 04:16:15 +0300

    chore: Add coverage directory to .gitignore and update test coverage scripts in package.json

--- packages/graz/vitest.config.ts
+++ packages/graz/vitest.config.ts
@@ -6,11 +6,6 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: ["./src/__tests__/setup.ts"],
-    coverage: {
-      provider: "v8",
-      reporter: ["text", "json", "html"],
-      exclude: ["node_modules/", "src/__tests__/", "**/*.d.ts", "**/*.config.*", "**/mockData", "dist/"],
-    },
   },
   resolve: {
     alias: {
