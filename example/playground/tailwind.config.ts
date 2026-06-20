60ce632ff588d86fe9fcc89597f9d1ba78f6f1b8
Griko Nibras <griko@nibras.co>
2026-06-21 02:35:44 +0700

    fix: update examples for modern deps

--- example/playground/tailwind.config.ts
+++ example/playground/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 const config: Config = {
   darkMode: ["class"],
@@ -51,7 +52,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
 
 export default config;
